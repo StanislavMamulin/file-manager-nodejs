8113804cf61391e47e31583760c20f3e7c8a61d9
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-20 17:41:53 +0500

    feat: add operation for change directory

--- src/operations/navigation.js
+++ src/operations/navigation.js
@@ -1,12 +1,25 @@
-import { normalize } from 'node:path';
+import { normalize, resolve } from 'node:path';
 import { getUserHomedir } from "../utils/path.js";
+import { checkCdPath } from './fs.js';
 
 let currentPath = getUserHomedir();
 
-export const showWhereIAm = () => { console.log(`You are currently in ${getCurrentPath()}`) };
+export const showWhereIAm = () => { console.log(`% You are currently in ${getCurrentPath()}`) };
 
 export const up = () => {
   currentPath = normalize(currentPath + '/..');
+};
+
+export const cd = async (newPath) => {
+  const wishPath = resolve(currentPath, newPath);
+
+  try {
+    await checkCdPath(wishPath);
+
+    currentPath = wishPath;
+  } catch(err) {
+    throw err;
+  }
 };
 
 export const getCurrentPath = () => currentPath;
