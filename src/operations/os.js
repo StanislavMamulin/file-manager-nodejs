6369aaa083049fdb8bae4550455009572c03c0f7
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-23 14:58:32 +0500

    refactor: change cpu numeration, os command parameters convert to case sensitive

--- src/operations/os.js
+++ src/operations/os.js
@@ -1,15 +1,15 @@
 import { EOL, cpus, homedir, userInfo, arch } from 'node:os';
 
 export const getOsInfo = async (infoType) => {
-  const type = infoType.slice(2).toLowerCase();
+  const type = infoType.slice(2)
   switch(type) {
-    case 'eol':
+    case 'EOL':
       return JSON.stringify(EOL);
     case 'cpus':
       const cpuInfo = cpus();
       const cpusAmount = cpuInfo.length;
       const cpusInfo = cpuInfo.map(
-        (info, index) => `CPU #${index}:\n Model: ${info.model}\n Speed: ${info.speed/1000} GHz`
+        (info, index) => `CPU #${index + 1}:\n Model: ${info.model}\n Speed: ${info.speed/1000} GHz`
       ).join('\n');
 
       return `Total CPU: ${cpusAmount}\n${cpusInfo}`;
