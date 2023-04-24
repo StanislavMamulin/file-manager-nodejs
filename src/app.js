5a687052d200e7ddfda3232cdb9c52d1d87170bb
Stanislav Mamulin <StanislavPM@ya.ru>
2023-04-24 19:34:44 +0500

    refactor: convert the switch to an object and move it to a separate file

--- src/app.js
+++ src/app.js
@@ -1,109 +1,22 @@
-import { cd, listFolderContent, showWhereIAm, up } from './operations/navigation.js';
 import { createInterface } from 'node:readline';
+import { showWhereIAm } from './operations/navigation.js';
 import { getUserName, showGoodbay, showGreeting } from './utils/user.js';
-import { catFile, copyMoveFile, createFile, deleteFile, renameFile } from './operations/files.js';
-import { checkNumberOfParameters } from './utils/helpers.js';
-import { getOsInfo } from './operations/os.js';
-import { calculateHash } from './operations/hash.js';
-import { compress, decompress } from './operations/compress.js';
+import { controller } from './controller.js';
 
 const inputHandler = async (line, rl) => {
   const [command, ...params] = line.split(' ');
+  if (command === '.exit') {
+    rl.close();
+    return;
+  }
+
   try {
-    switch (command) {
-      case '.exit':
-        rl.close();
-        return;
-      case 'up':
-        up();
-        break;
-      case 'cd':
-        await cd(params[0]);
-        break;
-      case 'cat':
-        checkNumberOfParameters(params, 1);
-
-        const [readFilePath] = params;
-        const filecontent = await catFile(readFilePath);
-        console.log(filecontent);
-  
-        break;
-      case 'add':
-        await createFile(params[0]);
-        break;
-      case 'rn':
-        checkNumberOfParameters(params, 2);
-
-        const [oldName, newName] = params;
-        await renameFile(oldName, newName);
-        break;
-      case 'cp':
-        checkNumberOfParameters(params, 2);
-
-        const [filepath, newDirectoryPath] = params;
-        await copyMoveFile({ filepath, newDirectoryPath, isMove: false });
-        break;
-      case 'mv':
-        checkNumberOfParameters(params, 2);
-
-        const [moveFilepath, moveToPath] = params;
-        await copyMoveFile({ filepath: moveFilepath, newDirectoryPath: moveToPath, isMove: true });
-        break;
-      case 'rm':
-        checkNumberOfParameters(params, 1);
-
-        const [filePath] = params;
-        await deleteFile(filePath);
-
-        break;
-      case 'os':
-        checkNumberOfParameters(params, 1);
-
-        const [neededInfo] = params;
-        const info = await getOsInfo(neededInfo);
-        console.log(info);
-
-        break;
-      case 'hash':
-        checkNumberOfParameters(params, 1);
-
-        const [hashFilePath] = params;
-        const hash = await calculateHash(hashFilePath);
-        console.log(hash);
-
-        break;
-      case 'compress':
-        checkNumberOfParameters(params, 2);
-
-        const [compressFilePath, archivePath] = params;
-        const result = await compress(compressFilePath, archivePath);
-        console.log(result);
-        break;
-      case 'decompress':
-        checkNumberOfParameters(params, 2);
-
-        const [archiveFilePath, destinationPath] = params;
-        const decompressResult = await decompress(archiveFilePath, destinationPath);
-        console.log(decompressResult);
-
-        break;
-      case 'ls':
-        checkNumberOfParameters(params, 0);
-
-        const folderContent = await listFolderContent();
-        console.table(folderContent);
-
-        break;
-      default:
-        console.log('Invalid input');
-        return;
-    }
+    await controller(command, params);
 
     showWhereIAm();
   } catch(err) {
     console.error(`Operation failed: ${err.message}`);
   }
-  
 }
 
 const app = () => {
